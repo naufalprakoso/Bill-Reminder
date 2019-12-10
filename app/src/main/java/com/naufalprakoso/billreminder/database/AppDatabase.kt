ed121ea72f4bc8caeb86d7b0f01d95c32f894444
naufalprakoso <naufalprakoso24@gmail.com>
2019-12-10 12:48:55 +0700

    Add App Database

---
+++ app/src/main/java/com/naufalprakoso/billreminder/database/AppDatabase.kt
@@ -0,0 +1,34 @@
+package com.naufalprakoso.billreminder.database
+
+import android.content.Context
+import androidx.room.*
+import com.naufalprakoso.billreminder.database.dao.BillDao
+import com.naufalprakoso.billreminder.database.entity.Bill
+
+@Database(entities = [Bill::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun BillDao(): BillDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java, "bill-reminder.db"
+                    )
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
+
+}
