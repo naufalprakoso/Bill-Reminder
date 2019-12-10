44bb06ebe737f4263b87f2e7ebc4784b85614e81
naufalprakoso <naufalprakoso24@gmail.com>
2019-12-10 12:35:43 +0700

    Add Bill entity

---
+++ app/src/main/java/com/naufalprakoso/billreminder/entity/Bill.kt
@@ -0,0 +1,23 @@
+package com.naufalprakoso.billreminder.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bills")
+data class Bill(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "content")
+    var content: String,
+
+    @ColumnInfo(name = "amount")
+    var amount: Int,
+
+    @ColumnInfo(name = "content")
+    var paid: Boolean
+)
